d0f163dda8a58a6e7394662ae9ff3d228234259d
Oleksandr Miliukhin <soapmintus@gmail.com>
2024-04-22 23:28:24 +0300

    memeow

--- math_research/lab/lab_5/script.r
+++ math_research/lab/lab_5/script.r
@@ -21,13 +21,7 @@ twho <- numerize(twho)
 hwho <- numerize(hwho)
 
 ideal_point <- function(x) {
-	# шукаєм координати ідеальної точки
-	# або 0,0,0 --- ідеальна точка, зручно ж
 	ideal_point <- c(0,0,0)
-	# for (i in 1:nrow(x)) {
-	# 	ideal_point[i] <- min(x[i,])
-	# }
-	# обчислюємо відстані
 	mindist <- Inf
 	distlist <- c()
 	for (i in 1:length(x)) {
@@ -35,34 +29,25 @@ ideal_point <- function(x) {
 		distlist[i] <- dist
 	}
 	x <- rbind(x,dist=distlist)
-	print(x)
-	print(which.min(x["dist",]))
 }
 
-cat("Розпізнавання символа T (ідеальна точка)\n")
-ideal_point(twho)
-cat("Розпізнавання символа H (ідеальна точка)\n")
-ideal_point(hwho)
-
-# точно зручно?
 maxmin <- function (x, quotients=c(1/3,1/3,1/3)) {
 	maxlist <- c()
 	cat("Коефіцієнти:", quotients, "\n")
-	# print(x[,1])
-	# print(quotients*x[,1])
 	for (i in 1:ncol(x)) {
-		# print(max(x[,i]))
 		maxlist[i] <- max(quotients * x[,i])
-		# maxlist[i] <- max(x[,i])
 	}
 	x <- rbind(x, max=maxlist)
-	print(x)
-	which.min(x["max",])
 }
 
+cat("Розпізнавання символа T (ідеальна точка)\n")
+write.csv(ideal_point(twho), "t_ideal_point.csv")
+cat("Розпізнавання символа H (ідеальна точка)\n")
+write.csv(ideal_point(hwho), "h_ideal_point.csv")
+
 cat("Розпізнавання символа T (максмінна згортка)\n")
-maxmin(twho)
-maxmin(twho, c(0.25,0.25,0.5))
+write.csv(maxmin(twho), "t_maxmin_1.csv")
+write.csv(maxmin(twho, c(0.25,0.25,0.5)), "t_maxmin_2.csv")
 cat("Розпізнавання символа H (максмінна згортка)\n")
-maxmin(hwho)
-maxmin(hwho, c(0.25,0.25,0.5))
+write.csv(maxmin(hwho), "h_maxmin_1.csv")
+write.csv(maxmin(hwho, c(0.25,0.25,0.5)), "h_maxmin_2.csv")
